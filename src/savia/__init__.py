4134b5d779faf9081b5f337532b7abf6be15bbd4
Dylan Gerardo Garbanzo Fallas <dgarbanzof@estudiantec.cr>
2026-06-09 10:16:39 -0600

    Update SAVIA system classes to include control logic and additional notifications

--- src/savia/__init__.py
+++ src/savia/__init__.py
@@ -1,6 +1,9 @@
-"""SAVIA system classes: sensors, actuators and telemetry."""
+"""SAVIA system classes: sensors, actuators, control logic and telemetry."""
 
 from .actuator import Actuator, Buzzer, Led, WaterPump
+from .application import Application
+from .controller import Controller
+from .notification import AlertType, Notification
 from .sensor import LightSensor, Sensor, SoilMoistureSensor, UltrasonicSensor
 from .telemetry import Telemetry, TelemetryReading
 
@@ -13,6 +16,10 @@ __all__ = [
     "SoilMoistureSensor",
     "LightSensor",
     "UltrasonicSensor",
+    "Controller",
+    "Notification",
+    "AlertType",
+    "Application",
     "Telemetry",
     "TelemetryReading",
 ]
